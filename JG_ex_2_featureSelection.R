38174e6c872c56c7b153a5dc1de34cbe31159b0c
jgalin <jgalin@ttu.ee>
2021-10-03 21:47:21 +0300

    Add files

---
+++ JG_ex_2_featureSelection.R
@@ -0,0 +1,219 @@
+# Exercise 2: Feature selection
+# Javier Galindos
+rm(list=ls())
+library("rstudioapi")  
+set.seed(123)
+# Hopkins statistics from scratch
+
+# Set working directory
+setwd(dirname(getActiveDocumentContext()$path)) 
+getwd()  
+
+# Load distance function
+source("JG_ex_1_distance.R")
+
+# loading the data from file
+load(file="Data/JGdata.RData")
+#load(file="Data/SmallData.RData")
+
+# Paramters for Hopkins Statistics
+sample_size <- 150
+distFun= "Euclidean"
+
+# Function
+JG_hopkinsStats = function(dataset, sample_size){
+  # sample of the dataset
+  ind <- sample(nrow(dataset), size = sample_size) # nrow() not working for one dimesion
+  dataset = dataset[ind,]
+  
+  distFun= "Euclidean"
+  
+  #1 Generating synthetic dataset
+  uniformGenerator = function(element){
+    return( runif(length(element),min(element),max(element)))
+  }
+  
+  syntheticData <- matrix(,nrow=nrow(dataset), ncol=ncol(dataset))
+  
+  for (i in seq(along=1:ncol(dataset))){
+    syntheticData[,i] = uniformGenerator(dataset[,i])
+  }
+  
+  #2 Compute distance to each nearest neighbor
+  distNN = function(element, dataset) {
+    dist <- matrix(,nrow = nrow(dataset))
+    for (i in seq(along=1:nrow(dataset))){
+      dist[i] <- JG_dist(element,dataset[i,],distFun)
+    }
+    dist <- dist[dist>0]
+    return(min(dist))
+  }
+  
+  # Sampled dataset
+  distSample <- matrix(,nrow = nrow(dataset))
+  for (i in seq(along=1:nrow(dataset))){
+    distSample[i] <- distNN(dataset[i,],dataset)
+  }
+  
+  # Synthetic dataset
+  distSynth <- matrix(,nrow = nrow(dataset))
+  for (i in seq(along=1:nrow(dataset))){
+    distSynth[i] <- distNN(syntheticData[i,],dataset)
+  }
+  
+  # Compute Hopkins stats
+  H = (sum(distSynth))/(sum(distSynth) + sum(distSample))
+}
+
+H = JG_hopkinsStats(x,sample_size)
+H 
+
+# R function
+library(clustertend)
+H.R = hopkins(x, sample_size)
+H.R
+
+# Entropy from scrach
+dataset <- x
+JG_entropy <- function(dataset) {
+  n_regions <- 10
+  
+  axis_grid <- matrix(,nrow = ncol(dataset), ncol= (n_regions+1))
+  #Create the regions
+  for (i in seq(along=1:ncol(dataset))){
+    axis_grid[i,] <- seq(from = min(dataset[,i]), to = max(dataset[,i]), by = (abs((max(dataset[,i]) - min(dataset[,i]))/ n_regions)))
+  }
+  
+  grid_ranges <- n_regions**ncol(dataset)
+  # Check number of points in each region (2D)
+  p <- array(0, dim = grid_ranges)
+  horz <- 1
+  vert <- 1 
+  for (i in seq(along=1:grid_ranges)){
+    x_min <- axis_grid[1,horz]
+    x_max <- axis_grid[1,horz+1]
+    y_min <- axis_grid[2,vert]
+    y_max <- axis_grid[2,vert+1]
+    # Check if each datapoint belong to this region
+    for (j in seq(along=1:nrow(dataset))){
+      x <- dataset[j,1]
+      y <- dataset[j,2]
+      if (x >= x_min && x <= x_max){ # > prev pointx && < next pointx
+        if (y >= y_min && y <= y_max){ # >prev point y && < next pointy
+          p[i] <- p[i] + 1
+        }
+      }
+    }
+    
+    # Update x and y
+    if (i %in% seq((n_regions),(grid_ranges - n_regions), n_regions)){
+      vert <- vert + 1 #Increment y
+      horz <- 0
+    }
+    horz <- horz + 1 #Increment x
+  }
+  
+  p <- p / nrow(dataset)
+  p <- p[p>0]
+  
+  #compute entropy
+  E <- -sum(p * log(p) + (1-p) * log(1-p))
+  return (E)
+}
+
+E = JG_entropy(x)
+
+## Feature selection
+
+# Load silhouette coefficient function
+source("JG_ex_5_silhouette.R")
+
+# Dataset
+load(file="Data/3gaussiandata.RData")
+library(rgl)
+plot3d(x[,1], x[,2], x[,3], type="p",col=(x[,4]+1)) 
+
+
+# Check Hopkins and Entropy in features 1 and 2
+E_12= JG_entropy(x[,1:2])
+Hopkins_12=JG_hopkinsStats(x[,1:2],sample_size)
+Sil_12 = JG_silhouette(x[,1:2],x[,4])
+
+# Check Hopkins and Entropy in features 2 and 3
+E_23= JG_entropy(x[,2:3])
+Hopkins_23=JG_hopkinsStats(x[,2:3],sample_size)
+Sil_23 = JG_silhouette(x[,2:3],x[,4])
+
+# Check Hopkins and Entropy in features 1 and 3
+E_13= JG_entropy(x[,c(1,3)])
+Hopkins_13=JG_hopkinsStats(x[,c(1,3)],sample_size)
+Sil_13 = JG_silhouette(x[,c(1,3)],x[,4])
+
+
+# 1 & 2 is the best option
+
+# K-means
+library(shotGroups)
+
+# Features 1 & 2
+train = x[,1:2]
+results <- kmeans(train,2)
+idx <- results$cluster
+plot(x[,1],x[,2], col= (idx+1),main='K-means: Feature 1 and 2')
+Sil_12_kmeans = JG_silhouette(train, idx)
+
+# Features 2 & 3
+train = x[,2:3]
+results <- kmeans(train,2)
+idx <- results$cluster
+plot(x[,2],x[,3], col= (idx+1),main='K-means: Feature 2 and 3')
+Sil_23_kmeans = JG_silhouette(train, idx)
+
+# Features 1 & 3
+train = x[,c(1,3)]
+results <- kmeans(train,2)
+idx <- results$cluster
+plot(x[,1],x[,3], col= (idx+1),main='K-means: Feature 1 and 3')
+Sil_13_kmeans = JG_silhouette(train, idx)
+
+# All features
+train = x[,1:3]
+results <- kmeans(train,2)
+idx <- results$cluster
+Sil_all_kmeans = JG_silhouette(train, idx)
+
+# EM Mixture of gaussians
+library(mixtools)
+
+# Features 1 & 2
+train = x[,1:2]
+gm<-mvnormalmixEM(train,k=2)  #multivariate normal distribution EM
+pred<-apply(gm$posterior, 1, function(row) which.max(row))
+plot(gm, which=2, main2='EM: Features 1 and 2')
+Sil_12_EM = JG_silhouette(train, pred)
+
+# Features 2 & 3
+train = x[,2:3]
+gm<-mvnormalmixEM(train,k=2)  #multivariate normal distribution EM
+pred<-apply(gm$posterior, 1, function(row) which.max(row))
+plot(gm, which=2,main2='EM: Features 2 and 3',xlim=c(-4,18),ylim=c(-4,13))
+Sil_23_EM = JG_silhouette(train, pred)
+
+# Features 1 & 3
+train = x[,c(1,3)]
+gm<-mvnormalmixEM(train,k=2)  #multivariate normal distribution EM
+pred<-apply(gm$posterior, 1, function(row) which.max(row))
+plot(gm, which=2,main2='EM: Features 1 and 3',xlim=c(-6.5,9.5),ylim=c(-2,12))
+Sil_13_EM = JG_silhouette(train, pred)
+
+# All features
+train = x[,1:3]
+gm<-mvnormalmixEM(train,k=2)  #multivariate normal distribution EM
+pred<-apply(gm$posterior, 1, function(row) which.max(row))
+#plot(gm, which=2)
+Sil_all_EM = JG_silhouette(train, pred)
+
+
+
+
+
---
+++ JG_ex_3_kmeans.R
@@ -0,0 +1,104 @@
+# Exercise 3: K-means
+# Javier Galindos
+
+# K-means without libraries
+# this file is to implement my own clustering 
+rm(list=ls())
+graphics.off()
+library("rstudioapi")  
+set.seed(123)
+
+# Set working directory
+setwd(dirname(getActiveDocumentContext()$path)) 
+getwd()  
+
+# loading the data from file
+load(file="Data/JGdata.RData")
+#load(file="Data/2gaussian.RData")
+
+# Load distance function
+source("JG_ex_1_distance.R")
+
+# function to find the label
+get_label <- function(dataset, centroids){
+  for (i in seq(along=1:nrow(dataset))){
+    temp_dist <- matrix(,nrow=nrow(centroids))
+    for (j in seq(along=1:nrow(centroids))){
+      temp_dist[j,] = JG_dist(centroids[j,], dataset[i,1:2], "Euclidean")
+    }
+    dataset[i, 3] = which.min(temp_dist)
+  }
+  return(dataset)
+}
+
+# update centroids on each iteration
+update_centroids <- function(dataset){
+  last_column = ncol(dataset)
+  K = max(dataset[,last_column])
+  centroids <- matrix(, nrow=K, ncol=last_column-1)
+  for (k in seq(along=1:K)){
+    cluster <- dataset[dataset[, last_column] == k, 1:2]
+    
+    centroids[k,] = colMeans(cluster)
+    
+  }
+  cat("___")
+  cat(centroids)
+  cat("___ \n")
+  return(centroids)
+}
+
+
+# do we need the following step?
+#split the data in proportion 70/30 for training and validation purposes.
+sample_size <- floor(0.7 * nrow(x))
+data_dimensionality = ncol(x)
+#set.seed(123) # seed is necessary to make it reproducible
+train_ind <- sample(seq_len(nrow(x)), size = sample_size)
+
+train_set <- x[train_ind, ]
+test <- x[-train_ind, ]
+train <- train_set[,1:2] # just to assure that we have two columns
+
+# initialize
+# Assume that K = 3
+K <- 3 
+centroids <- matrix(, nrow=K,ncol=data_dimensionality)
+
+
+for (k in seq(along=1:K)){
+  centroids[k,] = runif(2)
+}
+cat("Initial centroids are: ", centroids)
+
+# this example does not contain any convergence criteria
+# students are asked to design and add it.
+
+# add one column (to be used for labels)
+aux_column <- matrix(0, nrow(train_set))
+train_set = cbind(train_set,aux_column)
+
+# initialize 
+#centroids <- matrix(, nrow=K,ncol=data_dimensionality)
+step = 1
+
+# Add stopping criteria: 1. No variation in centroids or 2. More than 100 iterations
+while (step < 100){
+  cat("Step: ", step)
+  train_set <- get_label(train_set, centroids)
+  centroids_prev <- centroids
+  centroids <- update_centroids(train_set)
+  cat(" \n Centroids have been updated: ", centroids)
+  #plot(centroids, col="red")
+  #par(new=TRUE)
+  if (identical(centroids_prev, centroids)) {
+    break
+  }
+  step = step + 1
+}
+
+plot(train_set[, 1], train_set[, 2], type='p',col=(train_set[,3]+1),main='K-means output')
+par(new=TRUE)
+points(centroids[,1], centroids[,2], col="black", type='p',pch=20)
+
+plot(x[,1],x[,2], main = 'JG Dataset', pch= 20)
